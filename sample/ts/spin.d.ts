082aaf17b6de9a5bd4a390b6570c65b915f8df09
Toshiaki Inahata <darwin49@gmail.com>
2016-07-11 11:16:23 +0900

    clean sample/ts/*.js, sample/ts/*.js.map and sample/ts/*.d.ts

--- sample/ts/spin.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../../typings/react/react.d.ts" />
-import React = require("react");
-import { SpinProps } from "./sample";
-export declare class Spin extends React.Component<SpinProps, {}> {
-    constructor(prop: SpinProps);
-    render(): JSX.Element;
-}
