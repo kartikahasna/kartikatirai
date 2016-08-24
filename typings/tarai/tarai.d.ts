65cff0f9d2484d099ed599329a92411ab8d1e0fd
Toshiaki Inahata <darwin49@gmail.com>
2016-08-24 20:38:19 +0900

    0.0.15

--- typings/tarai/tarai.d.ts
+++ typings/tarai/tarai.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for tarai v0.0.14
+// Type definitions for tarai v0.0.15
 // Project: https://github.com/inabe49/tarai
 // Definitions by: inabe49 <https://github.com/inabe49>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
